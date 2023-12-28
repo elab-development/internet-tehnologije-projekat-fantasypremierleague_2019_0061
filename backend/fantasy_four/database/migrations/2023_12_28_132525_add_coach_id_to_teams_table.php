7734d2540b7c7fe84b571989550962be9d9b7d7d
Rashoola <petartheartist@gmail.com>
2023-12-28 14:30:06 +0100

    Added foreign keys

---
+++ backend/fantasy_four/database/migrations/2023_12_28_132525_add_coach_id_to_teams_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+           $table->unsignedBigInteger('coach_id');
+
+           $table->foreign('coach_id')->references('id')->on('coaches');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropForeign(['coach_id']);
+            $table->dropColumn('coach_id');
+        });
+    }
+};
