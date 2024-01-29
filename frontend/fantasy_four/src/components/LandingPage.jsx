fb900a1634035330d2e0533c1572a2b9a8b06737
Rashoola <petartheartist@gmail.com>
2024-01-29 18:49:08 +0100

    Added background change effect on the landing page, css modified

--- frontend/fantasy_four/src/components/LandingPage.jsx
+++ frontend/fantasy_four/src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import '../css/LandingPage.css'; // Import the CSS file
+import '../css/LandingPage.css';
 import NavigationMenu from './NavigationMenu';
 import CurrentGameDataField from './CurrentGameDataField';
 
@@ -8,16 +8,41 @@ const LandingPage = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const images = [
+    'photos/image1.jpeg',
+    'photos/image2.jpeg',
+    'photos/image3.jpeg',
+    'photos/image4.jpeg',
+    'photos/image5.jpeg',
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Update the current image index every 5 seconds
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 5000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [images.length]);
+
+  const backgroundStyle = {
+    backgroundImage: `url(${images[currentImage]})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+  };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    // Add logic to handle form submission (e.g., sending data to a server)
   };
 
   return (
     
-    <div className="container">
-      <NavigationMenu />
+    <div className="container" style={backgroundStyle}>
       <h1>Welcome to FantasyFour</h1>
       <form onSubmit={handleFormSubmit}>
         <label>
