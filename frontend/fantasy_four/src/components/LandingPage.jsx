dafeadd6929db55a3fbdda70434ac5e89ead42eb
Strahinja Jekic <jekic2409@gmail.com>
2024-01-30 13:40:15 +0100

    Form validation added

--- frontend/fantasy_four/src/components/LandingPage.jsx
+++ frontend/fantasy_four/src/components/LandingPage.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/LandingPage.css';
 import CurrentGameDataField from './CurrentGameDataField';
 import BackgroundImageRotator from './BackgroundImageRotator';
-import NavigationMenu from './NavigationMenu';
-
+ 
 const LandingPage = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -12,42 +11,37 @@ const LandingPage = () => {
  
   const handleFormSubmit = (event) => {
     event.preventDefault();
+ 
+    if (!name || !email || !password) {
+      alert('Please fill in all fields');
+      return;
+    }
+    alert('Form submitted successfully!');
   };
-
+ 
   return (
-    
     <BackgroundImageRotator>
-      <NavigationMenu></NavigationMenu>
-      <h1 style={{fontSize: '70px'}}>Welcome to FantasyFour</h1>
+      <h1 style={{ fontSize: '70px' }}>Welcome to FantasyFour</h1>
       <form onSubmit={handleFormSubmit}>
-        <label>
-          Name:
-        </label>
+        <label>Name:</label>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <br />
-        <label>
-          Email:
-
-        </label>
+        <label>Email:</label>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <br />
-        <label>
-          Password:
-
-        </label>
+        <label>Password:</label>
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <br />
-        <button type="submit" id='register-button'>Register</button>
+        <button type="submit" id='register-button'>
+          Register
+        </button>
       </form>
-      <div id='game-data'>
-      </div>
+      <div id='game-data'></div>
       <Link to="/login" id='link'>
         I already have an account, go to the login page
       </Link>
-
     </BackgroundImageRotator>
-    
   );
 };
-
+ 
 export default LandingPage;
